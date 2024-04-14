2a5f3c2bfc31d1d965fd22339cd70be1a61a7535
p1kalys <pavanemani14@gmail.com>
2024-04-14 15:10:08 +0530

    set and get

--- app/main.py
+++ app/main.py
@@ -2,40 +2,79 @@
 import socket
 import threading
 
-def read_from_parts(parts):
-    stack = []
-    while part := next(parts):
-        if part[0] == "*":
-            length = int(part[1::])
-            arr = []
-            for i in range(length):
-                middle = read_from_parts(parts)
-                arr.append(middle)
-            return arr
-        elif part[0] == "$":
-            length = int(part[1::])
-            if length == 1:
-                return None
-            part_string = next(parts)
-            return part_string
-        else:
-            return Exception("Invalid protocol")
+DB = {}
+
+def read_from_parts(msg):
+    str_msg = msg.decode()
+    tokens = str_msg.split("\r\n")
+    print(f"tokens: {tokens}")
+    parsed_msg = []
+    for idx, token in enumerate(tokens):
+        # bulk strings begin with $, so following value is the string
+        if "*" in token:
+            # array_token = tokens[idx+1]
+            array_len = token[1:]
+            print(f"array length: {array_len}")
+        if "$" in token:
+            bulk_string = tokens[idx + 1]
+            print(f"bulk string: {bulk_string}")
+            parsed_msg.append(bulk_string.lower())
+    cmd = parsed_msg[0]
+    msg_args = parsed_msg[1:]
+    return cmd, msg_args
 
 def handle_client(client):
     while True:
         data = client.recv(1024)
+        
         if not data:
             break
+        
         data_parts = iter(data.decode().split("\r\n"))
-        commands = read_from_parts(data_parts)
-        if commands[0].lower() == "ping":
-            response_data = b"+PONG\r\n"
-            client.sendall(response_data)
-        elif commands[0].lower() == "echo" and len(commands) > 1:
-            response_data = f"+{commands[1]}\r\n".encode()
-            client.sendall(response_data)
+        command, args_data = read_from_parts(data_parts)
+        
+        if command == "ping":
+            ping(client, args_data)
+        elif cmd == "echo":
+            echo(client, args_data)
+        elif cmd == "get":
+            get(client, args_data)
+        elif cmd == "set":
+            set(client, args_data)
+        else:
+            print("Command not supported!")
     
     client.close()
+
+def ping(client, args) -> None:
+    resp = b"+PONG\r\n"
+    client.sendall(resp)
+
+def echo(client, args) -> None:
+    msg = " ".join(args)
+    msg = f"+{msg}\r\n"  # format for simple string & CRLF
+    resp = msg.encode()
+    client.sendall(resp)
+
+def set(client, args) -> None:
+    key = args[0]
+    val = args[1]
+    try:
+        DB[key] = val
+    except:
+        print("DB insert failed")
+    resp = b"+OK\r\n"
+    client.sendall(resp)
+
+def get(client, args) -> None:
+    key = args[0]
+    try:
+        val = DB[key]
+    except:
+        print(f"No value at key: {key}")
+    msg = f"+{val}\r\n"
+    resp = msg.encode()
+    client.sendall(resp)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
