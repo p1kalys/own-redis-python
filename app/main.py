1bb21ea1ecb516d2ad40162dfb52ef7927708a0c
p1kalys <pavanemani14@gmail.com>
2024-04-14 11:53:43 +0530

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -10,7 +10,11 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     c, addr = server_socket.accept()
-    c.sendall(b"+PONG\r\n")
+    while(True):
+        data = c.recv(1024)
+        if not data:
+            break
+        c.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
