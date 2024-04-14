3147db35cd64f237fa6621a998b0ea31dca61940
p1kalys <pavanemani14@gmail.com>
2024-04-14 12:00:51 +0530

    multiple clients

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
+def handle_client(client):
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        client.sendall(b"+PONG\r\n")
+        client.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    c, addr = server_socket.accept()
-    while(True):
-        data = c.recv(1024)
-        if not data:
-            break
-        c.sendall(b"+PONG\r\n")
+    
+    while True:
+        client_socket, addr = server_socket.accept()
+        print('Connection from', addr)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket))
+        client_thread.start()
+
 
 if __name__ == "__main__":
     main()
